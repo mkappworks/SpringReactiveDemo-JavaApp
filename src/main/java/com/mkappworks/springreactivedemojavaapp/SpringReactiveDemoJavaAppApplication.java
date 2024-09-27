6ad266ec0e50c0432287f3013be2140b339f3aeb
Malith Kuruppu <info.mkappworks@gmail.com>
2024-09-27 16:19:05 +0200

    Initial Commit

---
+++ src/main/java/com/mkappworks/springreactivedemojavaapp/SpringReactiveDemoJavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.mkappworks.springreactivedemojavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveDemoJavaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveDemoJavaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mkappworks/springreactivedemojavaapp/SpringReactiveDemoJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkappworks.springreactivedemojavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveDemoJavaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
